f973d85e854f66131106b4d88bd8c33f95897123
Hasan <mr.hasan2004@mail.ru>
2024-12-16 16:54:00 +0500

    handle user create

--- handler_user_create.go
+++ handler_user_create.go
@@ -1,7 +1,54 @@
 package main
 
-import "net/http"
+import (
+	"encoding/json"
+	"net/http"
+	"time"
+
+	"github.com/google/uuid"
+	"github.com/imhasandl/go-restapi/internal/database"
+)
+
+type User struct {
+	ID        uuid.UUID
+	CreatedAt time.Time
+	UpdatedAt time.Time
+	Email     string
+}
 
 func (cfg *apiConfig) handlerUserCreate(w http.ResponseWriter, r *http.Request) {
+	type parameters struct {
+		ID uuid.UUID `json:"id"`
+		Email string `json:"email"`
+	}
+	type response struct {
+		User
+	}
 
+	decoder := json.NewDecoder(r.Body)
+	params := parameters{}
+	err := decoder.Decode(&params)
+	if err != nil {
+		respondWithError(w, http.StatusBadRequest, "Can't create user", err)
+		return
+	}
+
+	userCreateResponse := parameters{
+		ID: uuid.New(),
+		Email: params.Email,
+	}
+
+	user, err := cfg.db.CreateUser(r.Context(), database.CreateUserParams(userCreateResponse))
+	if err != nil {
+		respondWithError(w, http.StatusBadRequest, "Can't create user", err)
+	}
+
+	respondWithJSON(w, http.StatusOK, response{
+		User: User{
+			ID: user.ID,
+			CreatedAt: user.CreatedAt,
+			UpdatedAt: user.UpdatedAt,
+			Email: user.Email,
+		},
+	})
 }
